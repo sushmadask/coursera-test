927194114fbb1614623d7d24e53aff0e0564c1b8
sushma <sushmakdas@gmail.com>
2017-01-08 21:16:04 +0530

    module1-solution

---
+++ app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+  $scope.checkIfTooMuch = function () {
+      if($scope.listData != undefined && $scope.listData.length > 0){
+         
+          var list = $scope.listData.split(',');
+          var filteredArray = [];
+            angular.forEach(list, function(item) {
+              if (item) filteredArray.push(item);
+            });
+          if(filteredArray.length <= 3){
+            $scope.message = 'Enjoy!';
+          }else{
+            $scope.message = 'Too much!';
+          }
+      }
+  }; 
+}
+
+})();
